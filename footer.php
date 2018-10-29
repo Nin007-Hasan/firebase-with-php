11b6d186474457c0da18803fb02813838f7e1f82
hihasan <hasan@hihasan.com>
2018-10-29 13:18:24 +0600

    initial files

---
+++ footer.php
@@ -0,0 +1,59 @@
+ <footer id="footer">
+    <div class="top-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 col-sm-4 marb20">
+            <div class="ftr-tle">
+              <h4 class="white no-padding">About Us</h4>
+            </div>
+            <div class="info-sec">
+              <p>Praesent convallis tortor et enim laoreet, vel consectetur purus latoque penatibus et dis parturient.</p>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-4 marb20">
+            <div class="ftr-tle">
+              <h4 class="white no-padding">Quick Links</h4>
+            </div>
+            <div class="info-sec">
+              <ul class="quick-info">
+                <li><a href="#"><i class="fa fa-circle"></i>Home</a></li>
+                <li><a href="#service"><i class="fa fa-circle"></i>Service</a></li>
+                <li><a href="#contact"><i class="fa fa-circle"></i>Appointment</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-4 marb20">
+            <div class="ftr-tle">
+              <h4 class="white no-padding">Follow us</h4>
+            </div>
+            <div class="info-sec">
+              <ul class="social-icon">
+                <li class="bglight-blue"><i class="fa fa-facebook"><a href="facebook.com" target="_blank"></a></i></li>
+                <li class="bgred"><a href="google.com target="_blank"><i class="fa fa-google-plus"></a></i></li>
+                <li class="bgdark-blue"><i class="fa fa-linkedin"><a href="linkedin.com"></a></i></li>
+                <li class="bglight-blue"><i class="fa fa-twitter"><a href="twitter.com"></a></i></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer-line">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            © Copyright Smart Health Care. All Rights Reserved
+            <div class="credits">
+              <!--
+                All the links in the footer should remain intact.
+                You can delete the links only if you purchased the pro version.
+                Licensing information: https://bootstrapmade.com/license/
+                Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Medilab
+              -->
+              Designed for <a href="#">North South University</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
---
+++ head.php
@@ -0,0 +1,12 @@
+<meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <title>Smart Health Care</title>
+ <meta name="description" content="Free Bootstrap Theme by BootstrapMade.com">
+ <meta name="keywords" content=" a
+system that will help pregnant women to do regular checkup without going anywhere and
+anyone’s help">
+
+ <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway|Candal">
+ <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+ <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+ <link rel="stylesheet" type="text/css" href="css/style.css">
