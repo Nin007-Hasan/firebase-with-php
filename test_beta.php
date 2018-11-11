7b7385a65296c06fbdec74c054b3d99dcb879fe6
unifoxdigital <unifoxdigital.hihasan@gmail.com>
2018-11-11 16:12:41 +0600

    setup firebase sdk

--- test_beta.php
+++ test_beta.php
@@ -3,4 +3,16 @@
 
 	use Kreait\Firebase\Factory;
 	use Kreait\Firebase\ServiceAccount;
+
+	// This assumes that you have placed the Firebase credentials in the same directory
+	// as this PHP file.
+	$serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/application/key/cse499healthmanagement-1e4bd5cf7cbb.json');
+
+	$firebase = (new Factory)
+	    ->withServiceAccount($serviceAccount)
+	    ->create();
+
+	$database = $firebase->getDatabase();
+
+	die(print_r($database));
 ?>
