0c8dd2efb2f0a1ba8629d0381f1025703f530c5c
hihasan <hasan@hihasan.com>
2018-12-19 13:20:23 +0600

    doctor profile

--- doctor/pages/profile_edit.php
+++ doctor/pages/profile_edit.php
@@ -14,5 +14,35 @@
 	  } 
 
 	  $doctor_email= $_SESSION['doctor_email'];
-	  echo "$doctor_email";
+	  //echo "$doctor_email";
+
+	  if (isset($_POST['name'])) {
+ 	# code...
+ 		$name = $_POST['name'];
+	 }
+
+	 if (isset($_POST['specialization'])) {
+ 	# code...
+ 		$specialization = $_POST['specialization'];
+	 }
+
+	 if (isset($_POST['password'])) {
+ 	# code...
+ 		$password = $_POST['password'];
+	 }
+
+	 $sql_Query= "UPDATE doctor_profile set designation='$designation', name='$name', specialization='$specialization', password='$password' WHERE doctor_email='$doctor_email' ";
+
+	 if(mysqli_query($con,$sql_Query)){
+
+	 	header("Location: index.php");
+		die();
+	 
+	 }
+	 else{
+	 
+	 echo mysql_error();
+	 
+	 }
+	 mysqli_close($con);
 ?>
